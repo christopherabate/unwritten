e9b55e5d48f8fc86e2ad7cfe56bf3890aa3cee64
Christopher Abate <contact@christopherabate.com>
2026-06-08 17:19:51 +0200

    Delete src directory

--- src/types/event.ts
+++
@@ -1,8 +0,0 @@
-import { Thought } from "./thought";
-
-export type Event =
-    | { type: "DRAW"; count: number }
-    | { type: "SELECT_INTENT" }
-    | { type: "PLAY_THOUGHT"; id: Thought['id'] }
-    | { type: "PLAY_INTENT" }
-    | { type: "CHECK_STABILITY" };
